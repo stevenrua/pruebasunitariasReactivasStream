765ed7af7162c342d39d3a7bed4aaa75c1c78b06
Steven Rúa Jaramillo <steven.rua@sofka.com.co>
2023-03-23 15:08:14 -0500

    This is a new commit for what I originally planned to be amended

--- src/test/java/com/example/demo/DojoReactiveTest.java
+++ src/test/java/com/example/demo/DojoReactiveTest.java
@@ -2,8 +2,17 @@ package com.example.demo;
 
 
 import org.junit.jupiter.api.Test;
+import org.w3c.dom.ls.LSOutput;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.GroupedFlux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
 
+import java.time.Duration;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 
 public class DojoReactiveTest {
 
@@ -17,30 +26,63 @@ public class DojoReactiveTest {
 
     @Test
     void jugadoresMayoresA35(){
+        List<Player> list = CsvUtilFile.getPlayers();
+        Flux.fromIterable(list)
+                .filter(player -> player.age >=35)
+                .groupBy(Player::getClub)
+                .subscribe(group -> {
+                    System.out.println("Equipo " + group.key() + ":");
+                    group.subscribe(p -> System.out.println("--" + p.getName() + " " + p.getAge()));
+                });
 
     }
 
     @Test
     void mejorJugadorConNacionalidadFrancia(){
+        List<Player> list = CsvUtilFile.getPlayers();
+        Flux.fromIterable(list)
+                .filter(player -> player.getNational().equals("France"))
+                .reduce((player1, player2) -> player1.getWinners() >=
+                        player2.getWinners() ? player1 : player2)
+                .subscribe(player -> System.out.println("El mejor jugador de francia es: " + player.getName()));
 
     }
 
     @Test
     void clubsAgrupadosPorNacionalidad(){
-
+        List<Player> list = CsvUtilFile.getPlayers();
+        Flux.fromIterable(list)
+                .groupBy(Player::getNational)
+                .subscribe(group -> {
+                    System.out.println("Clubes de " + group.key() + ":");
+                    group.subscribe(p -> System.out.println("_ " + p.getClub()));
+                });
     }
 
     @Test
     void clubConElMejorJugador(){
-
+        List<Player> list = CsvUtilFile.getPlayers();
+        Flux.fromIterable(list)
+                .reduce((jugador1, jugador2) -> jugador1.getWinners() >=
+                        jugador2.getWinners() ? jugador1 : jugador2)
+                .subscribe(bestplayer -> System.out.println("El mejor jugador es: " + bestplayer.getName() +
+                        " del país de " + bestplayer.getNational()));
     }
 
 
     @Test
     void mejorJugadorSegunNacionalidad(){
+        List<Player> list = CsvUtilFile.getPlayers();
+        Flux.fromIterable(list)
+                .groupBy(Player::getNational)
+                .flatMap(bestplayernation ->
+                        bestplayernation.reduce((j1, j2) -> j1.getWinners() >= j2.getWinners() ? j1 : j2))
+                .map(bestplayer -> {
+                    System.out.println("El mejor jugador de " + bestplayer.getNational() + " es :");
+                    System.out.println("--" + bestplayer.getName());
+                    return bestplayer;
+                }).subscribe();
 
     }
-
-
 
 }
--- src/test/java/com/example/demo/DojoStreamTest.java
+++ src/test/java/com/example/demo/DojoStreamTest.java
@@ -2,6 +2,9 @@ package com.example.demo;
 
 
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.GroupedFlux;
+
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -16,42 +19,63 @@ public class DojoStreamTest {
     @Test
     void jugadoresMayoresA35(){
         List<Player> list = CsvUtilFile.getPlayers();
-        Map<String, List<Player>> listFilter = list.parallelStream()
+        Map<String, List<Player>> groupedPeople = list.stream()
                 .filter(player -> player.age >= 35)
-                .map(player -> {
-                    player.name = player.name.toUpperCase(Locale.ROOT);
-                    return player;
-                })
-                .flatMap(playerA -> list.parallelStream()
-                        .filter(playerB -> playerA.club.equals(playerB.club))
-                )
-                .distinct()
                 .collect(Collectors.groupingBy(Player::getClub));
 
-        assert listFilter.size() == 322;
+        groupedPeople.forEach((club, playerList) -> {
+            System.out.println("Club: " + club);
+            playerList.forEach(player -> System.out.println(player.getName() + " " + player.getAge()));
+        });
 
     }
 
     @Test
     void mejorJugadorConNacionalidadFrancia(){
+        List<Player> list = CsvUtilFile.getPlayers();
+        Optional<Player> listaNueva = list.stream()
+                .filter(player -> player.getNational().equals("France"))
+                .max(Comparator.comparingInt(Player::getWinners));
+        System.out.println("El mejor jugador de francia es: " + listaNueva.get().getName()
+                + " con " + listaNueva.get().getWinners() + " partidos ganados");
 
     }
 
     @Test
     void clubsAgrupadosPorNacionalidad(){
+        List<Player> list = CsvUtilFile.getPlayers();
+        Map<String, List<Player>> groupedPeople = list.stream()
+                .collect(Collectors.groupingBy(Player::getNational));
 
+        groupedPeople.forEach((nacionalidad, clubes) -> {
+            System.out.println("Clubes de " + nacionalidad + ":");
+            clubes.forEach(player -> System.out.println("_ " + player.getClub()));
+        });
     }
 
     @Test
     void clubConElMejorJugador(){
+        List<Player> list = CsvUtilFile.getPlayers();
+        Optional<Player> bestPlayer = list.stream()
+                .max(Comparator.comparing(Player::getWinners));
 
+        System.out.println("El club con el mejor jugador es: " +
+                bestPlayer.get().getClub() + " y es " + bestPlayer.get().getName());
     }
 
 
     @Test
     void mejorJugadorSegunNacionalidad(){
+        List<Player> list = CsvUtilFile.getPlayers();
+        Map<String, Player> mejoresJugadoresPorEquipo = list.stream()
+                .collect(Collectors.groupingBy(Player::getNational,
+                        Collectors.collectingAndThen(
+                                Collectors.maxBy(Comparator.comparing(Player::getWinners)),
+                                Optional::get)));
 
+        mejoresJugadoresPorEquipo.forEach((nacionalidad, jugador) -> {
+            System.out.println("El mejor jugador de  " + nacionalidad + " es: " + jugador.getName());
+
+        });
     }
-
-
 }
